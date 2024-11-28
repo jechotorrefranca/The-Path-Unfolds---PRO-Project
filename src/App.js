2511dd7e537c84294f6baf20ccd37bc6b4c4db79
Kouji <Kouji@DESKTOP-VO9O7C0>
2024-11-28 19:21:23 +0800

    Tailwind Acquired

--- src/App.js
+++ src/App.js
@@ -3,21 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+      <div className="bg-white shadow-md rounded-lg p-8 flex flex-col justify-center items-center">
+        <h1 className="text-2xl font-bold text-gray-800">The Path Unfolds</h1>
+        <p className="text-gray-600">Begins Here</p>
+      </div>
     </div>
   );
 }
